a6c62385d1ec125c5f065ab1d6dcc818fca14d8f
Hexarion23 <kakefortytwo@gmail.com>
2025-05-15 12:59:47 +0800

    Created main menu

--- src/StudentLibrary.java
+++ src/StudentLibrary.java
@@ -1,8 +1,25 @@
+import javax.swing.JOptionPane;
+import student_system.StudentManagement;
+
 public class StudentLibrary {
     public static void main(String[] args) {
         String mainPrompt = "Enter your option: " +
                 "\n\n1. Student management" +
                 "2. Books in library" +
                 "3. Exit\n";
+
+        String enteredNumberForMainMenu = JOptionPane.showInputDialog(
+                null,
+                "Enter your option:\n" + 
+                "1. Student management\n" +
+                "2. Books in library\n" +
+                "3. Exit");
+
+        int option = Integer.parseInt(enteredNumberForMainMenu);
+        switch(option) {
+            case 1:         StudentManagement.main(args);
+            // case 2: BookManagement.
+        }
+
     }
 }
